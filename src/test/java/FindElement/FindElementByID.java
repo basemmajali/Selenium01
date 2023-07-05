71d2c5d2b80a73598e549bbfe21102fef7b93463
basem majali <91624634+basemmajali@users.noreply.github.com>
2023-07-05 15:19:12 +0300

    selenium01

---
+++ src/test/java/FindElement/FindElementByID.java
@@ -0,0 +1,24 @@
+package FindElement;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+
+public class FindElementByID {
+    public static void main(String[] args) {
+        //setup driver
+        WebDriverManager.edgedriver().setup();
+        //create object to start test on Edge
+        WebDriver driver =new EdgeDriver();
+        //go to this url
+        driver.get("https://hatem-hatamleh.github.io/Selenium-html/");
+        //based on previous url find element by this id (welcome)and print the text
+        System.out.println(driver.findElement(By.ById.id("welcome")).getText());
+        driver.quit();
+    }}
+//
+//    git remote add origin https://github.com/basemmajali/Selenium01.git
+//        git branch -M main
+//        git push -u origin main
